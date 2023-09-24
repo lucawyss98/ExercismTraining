bdb8f073f4fddf5e956eb77f4dc03b06e1e9fe70
lucawyss98 <luca.wyss98@hotmail.com>
2023-09-25 00:03:34 +0200

    Bowling game

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -3,21 +3,29 @@ import java.util.List;
 
 class BowlingGame {
 
-    private List<Frame> frames;
-
-    BowlingGame(){
-        frames = new ArrayList<>();
-    }
+    private int[][] rolls = new int[11][2];
+    private int frame = 0;
+    private int roll = 0;
 
     void roll(int pins) {
-        ;
+        if (roll > 1){
+            frame++;
+            roll=0;
+        }
+        if(frame > 11)
+            throw new IllegalStateException("Cannot roll after game is over");
+
+        rolls[frame][roll] = pins;
+        roll++;
+
+        if (pins == 10)
+            roll++;
     }
 
     int score() {
 
+
     }
 }
 
-class Frame{
 
-}
